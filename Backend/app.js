95e818d3b24a85ef2ae03e7bf82e500df7aae9bc
Alok Kumar <alokpkumar0023@gmail.com>
2024-01-31 05:03:44 +0530

    file creation

--- Backend/app.js
+++ Backend/app.js
@@ -1,36 +1,17 @@
+// app.js
 const express = require('express');
-const pool = require('./db'); // Assuming your db.js is in the same directory
+const authRoutes = require('./authRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// app.get('/users', async (req, res) => {
-//   try {
-//     const { rows } = await pool.query('SELECT * FROM users');
-//     res.json(rows);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-app.get('/users', async (req, res) => {
-    console.log('Received GET request to /users');
-    try {
-      const { rows } = await pool.query('SELECT * FROM users');
-      res.json(rows);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
-  });
-  
-
+});
